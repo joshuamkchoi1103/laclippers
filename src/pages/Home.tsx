c15cc1d2fa7b6de6da7ebb606489fcd6b74b93f4
Joshua  Choi <joshuamkchoi@Joshuas-MacBook-Pro.local>
2025-07-23 12:13:38 -0700

    Setup GitHub Pages deployment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import HeroBanner from '../components/HeroBanner';
 //import NextGame from '../components/NextGame';
 
-import '../styles/HeroBanner.css';
 import '../styles/NextGame.css';
-import '../styles/Home.css'
+import '../styles/Home.css';
+import backgroundImage from '../assets/court.png';
 
 const Home = () => {
   return (
-    <div>
-      <HeroBanner />
-           
+    <div className="hero-banner">
+      <img src={backgroundImage} alt="Intuit Dome court" />
+      <div className="overlay">
+        <h1>Welcome to ClippersCentral</h1>
+        <p>Your hub for roster, news & game insights.</p>
+      </div>
     </div>
+
   );
 };
 
